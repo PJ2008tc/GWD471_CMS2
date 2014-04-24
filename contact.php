5746506fbd98876b1ec29f444d777ec0b52215d0
PJ2008tc <PJ2008tc@stu.aii.eedu>
2014-04-24 01:38:19 +0000

    new upload

--- contact.php
+++ contact.php
@@ -1,32 +1,95 @@
- 
+
+   <?php
+
+      if(@$_POST['submit'])
+      {
+        require_once('includes/validation.php');
+        
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $comments = $_POST['comments'];
+        $errors = array();
+        
+        //test user info for non-valid data
+        $valid = verifyAlphaNum($name);
+        if(!$valid)
+        {
+           $errors[] = "name must be letters, numbers, or spaces"; 
+        }
+        
+        $valid = verifyEmail($email);
+        if(!$valid)
+        {
+           $errors[] = "Please provide a valid email address - test@test.com"; 
+        }
+        
+        $message = verifyText($comments);
+	
+	//if any errors, don't send email - else send email
+	if(count($errors) === 0)
+	{
+		//prepare for sending email
+		$destination = 'PJ2008tc@stu.aii.edu';
+		$subject = 'Comments from my website';
+		$body = "$name \n $email \n $comments";
+		
+		if(mail($destination, $subject, $body))
+		{
+			echo 'Your message has been sent. Below is the info you provided:';
+			
+			//test to see if form data is recieved.
+			echo '<h1>Thanks for providing feedback</h1>';
+			echo '<ul>';
+			echo '<li>' . $_POST['name'] . '</li>';
+			echo '<li>' . $_POST['email'] . '</li>';
+			echo '<li>' . $_POST['comments'] . '</li>';
+			echo '</ul>';
+		}
+		else
+		{
+			echo 'There was an error. Your message has NOT been sent.';
+		}
+	}//if count end
+
+        }
+        //else //form not submitted
+        {
+          echo 'Just view form';
+        }
+   ?>
+
   <?php $page_id = 'contact'; ?>
   <?php require_once('includes/top.php'); ?>
   <?php require_once('includes/header.php'); ?>
 
-  <form id="demo" action="/courses/imd315/form.php" method="post">
-		<fieldset>
-        <h2>Contact Support and Customer Service</h2>
-        <h3>How Can We Help You?</h3>
-        <p>Fill out all empty fields with your information and questions and we will contact you as soon as possible</p>
-			<p>
-				<label for="name">Name:</label>				<input type="text" id="name" name="name" value="" class="required" placeholder="please add you input here />
-			</p>
-			<p>
-				<label for="email">Email:</label>				<input type="text" id="email" name="email" value="" class="email required" />
-			</p>
-			<p>
-				<label for="message">Message:</label>				<textarea cols="45" rows="7" id="message" name="message" class="required"></textarea>
-			</p>
-			<input name="submitted" type="submit" value="Send" />
-		</fieldset>
-	</form>
-
-    <section>
+   <section>
     	<h2>Contact</h2>
-        <p>
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-        </p>
-    </section>
+     <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+         <fieldset>
+           <legend>Contact Us</legend>
+           <p>
+           <label for="name">Name: </label>
+           <input type="text" name="name" placeholder="Type your name here"/>
+           </p>
+           
+           <p>
+           <label for="email">Email: </label>
+           <input type="text" name="email" placeholder="Email"/>
+           </p>
+           
+           <p>
+           <label for="email">Comments: </label>
+           <textarea name="comments" placeholder="type your message here"></textarea>
+           </p>
+           
+           <p>
+           <input type="submit" value="send feedback now" name="submit" />
+           </p>
+         </fieldset>
+    </section>  
+
+   
+   
    
   <?php require_once('includes/footer.php'); ?>
    
